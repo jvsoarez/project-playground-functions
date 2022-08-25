f15b2988a882873fe213ff5bc9f3043bdba90848
jvsoarez <joaovictorcpsa@gmail.com>
2022-08-24 23:17:53 -0300

    Req 12 - done

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,8 +15,15 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+// eslint-disable-next-line complexity
+function triangleCheck(lineA, lineB, lineC) {
+  let differenceAtoB = Math.abs(lineA - lineB);
+  let differenceAtoC = Math.abs(lineA - lineC);
+  let differenceBtoC = Math.abs(lineB - lineC);
+  if (lineA < lineB + lineC && lineA > differenceBtoC) return true;
+  if (lineB < lineA + lineC && lineC > differenceAtoC) return true;
+  if (lineC < lineA + lineB && lineC > differenceAtoB) return true;
+  return false;
 }
 
 // Desafio 13
