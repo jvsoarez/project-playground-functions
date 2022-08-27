8770494cc1aca74f11d3351c7255713657221dcb
jvsoarez <joaovictorcpsa@gmail.com>
2022-08-27 01:13:24 -0300

    Req 11 and finish all functions

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,9 +10,27 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// eslint-disable-next-line complexity
+function generatePhoneNumber(arr) {
+  if (arr.length !== 11) return 'Array com tamanho incorreto.';
+  for (let i = 0; i < arr.length; i += 1) {
+    let repeatedNums = 0;
+    for (let j = 0; j < arr.length; j += 1) {
+      if (arr[i] === arr[j]) {
+        repeatedNums += 1;
+      }
+      if (arr[i] < 0 || arr[i] > 9 || repeatedNums >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+  }
+  let phoneNumber = `(${arr[0]}${arr[1]}) `;
+  phoneNumber += `${arr[2]}${arr[3]}${arr[4]}${arr[5]}${arr[6]}-`;
+  phoneNumber += `${arr[7]}${arr[8]}${arr[9]}${arr[10]}`;
+  return phoneNumber;
 }
+
+console.log(generatePhoneNumber([9, 2, 3, 5, 8, 9, 1, 3, 4, 2, 6]));
 
 // Desafio 12
 // eslint-disable-next-line complexity
